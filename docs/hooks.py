e62441aa3840562172e7fcd2098d6bac152896ff
Arielle R Munters <45485665+elleira@users.noreply.github.com>
2023-09-13 15:22:37 +0200

    docs: add changelog to doc hooks.py

--- docs/hooks.py
+++ docs/hooks.py
@@ -2,7 +2,7 @@ import shutil
 
 
 def copy_changelog_and_license(*args, **kwargs):
-    # shutil.copy("CHANGELOG.md", "docs/changelog.md")
+    shutil.copy("CHANGELOG.md", "docs/changelog.md")
     shutil.copy("LICENSE.md", "docs/license.md")
     shutil.copy("config/config.yaml", "docs/includes/config.yaml")
     shutil.copy("config/config_references.yaml", "docs/includes/config_references.yaml")
